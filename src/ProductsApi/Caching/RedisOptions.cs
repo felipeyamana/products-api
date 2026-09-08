a3f51fd408bc7aeaa3c3c872aab44c30bd57968b
felipeyamana <felipedias0810@gmail.com>
2026-09-08 18:16:09 -0300

    adding easy way to disable redis in production without re-deployment
    redis keeps disabling my free tier database, and getting 30 seconds timeout because my app couldnt reach redis is slowing down development

--- src/ProductsApi/Caching/RedisOptions.cs
+++ src/ProductsApi/Caching/RedisOptions.cs
@@ -3,12 +3,11 @@ namespace ProductsApi.Caching;
 public sealed class RedisOptions
 {
     public const string SectionName = "Redis";
-
     public bool Enabled { get; init; }
-
     public bool RegisterNullCacheWhenDisabled { get; init; }
-
     public string ConnectionString { get; init; } = string.Empty;
-
     public string InstanceName { get; init; } = "products-api:";
+    public int ConnectTimeoutMilliseconds { get; set; } = 2000;
+    public int SyncTimeoutMilliseconds { get; set; } = 2000;
+    public int AsyncTimeoutMilliseconds { get; set; } = 2000;
 }
